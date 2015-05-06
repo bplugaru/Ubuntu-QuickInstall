10dcfbe73439995b4cd40ea765a03de4cee1e690
Bogdan Plugaru <bplugaru@gmail.com>
2015-05-06 15:22:57 +0300

    add mongo

--- install.sh
+++ install.sh
@@ -19,5 +19,19 @@ Install_Node() {
 	echo "[-] Install Bower"
 	npm install bower -g	
 }
-Upddate_System
-Install_Node
+
+Install_Git() {
+	echo "[-] Install GIT"
+	apt-get install git		
+}
+Install_MongoDB() {
+	echo "[-] Install MONGO"
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+	echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+	apt-get install mongodb-org -y
+	service mongod start
+}
+#Upddate_System
+#Install_Node
+#Install_Git
+Install_MongoDB
