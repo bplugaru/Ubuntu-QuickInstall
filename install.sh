41b3bb4f5b6476cb0ab8722adb34c4e354f60867
Bogdan Plugaru <bplugaru@gmail.com>
2015-05-11 21:54:55 +0300

    add download file

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
- 
+#!/bin/bash
+
 Upddate_System() {
-	
+
 	echo "[-] Updating Packages"
 	apt-get update -y
-	
+
 	echo "[-] Upgrading Packages"
 	apt-get upgrade -y
 }
@@ -15,27 +15,32 @@ Install_Node() {
 	echo "[-] Install NPM"
 	apt-get install npm
 	echo "[-] Install PM2"
-	npm install pm2 -g	
+	npm install pm2 -g
 	echo "[-] Install Bower"
-	npm install bower -g	
+	npm install bower -g
 }
 
 Install_Git() {
 	echo "[-] Install GIT"
-	apt-get install git		
+	apt-get install git
 }
 Install_MongoDB() {
-	echo "Step 1:  Import the MongoDB public key" 
+	echo "Step 1:  Import the MongoDB public key"
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-	
+
 	echo "Step 2: Generate a file with the MongoDB repository url"
 	echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-	
+
 	echo "Step 3: Refresh the local database with the packages"
 	apt-get update
-	
+
 	echo "Step 4: Install the last stable MongoDB version and all the necessary packages on our system"
 	apt-get install mongodb
+}
+
+Install_Cmd() {
+	mkdir -p ~/bin
+	curl "https://raw.githubusercontent.com/bplugaru/Ubuntu-QuickInstall/master/cmd.sh" > ~/bin/cmd
 }
 #Upddate_System
 #Install_Node
